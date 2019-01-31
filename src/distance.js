c0583b9990344eb65773b08b669fcbad8c2c2124
Francois <flemmens@gmail.com>
2019-02-01 00:27:18 +0100

    Create distance.js

---
+++ src/distance.js
@@ -0,0 +1,38 @@
+/*
+    Compute the great-circle distance between two geographic points
+
+    (C) 2019 by GeoPostcodes - GeoData Ltd
+
+
+    The formula used to calculate the distance between two geographical points is called orthodromy, 
+    and is defined as follows:
+    
+    Distance(A,B) = 6371 * ACos( Cos(LatA) * Cos(LatB) * Cos(LngB - LngA) + Sin(LatA) * Sin(LatB) )
+    
+    The resulting number is the distance in kilometers between point A and point B. If you wish to 
+    return a result in miles (or any other unit), you must substitute the value 6371, which stands
+    for the approximate radius of the Earth in km, by its equivalent in miles (or any other unit),
+    that is, 3959 miles. Latitude and longitude values must be input in radians : radians = degrees * PI/180.
+
+*/
+
+function distance(from_lat, from_lng, to_lat, to_lng)
+{
+  var R = 6371; // Radius of the earth in km, can be changed to 3959 for miles
+  
+  var dLat = deg2rad(to_lat - from_lat);
+  var dLon = deg2rad(to_lng - from_lng); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(from_lat)) * Math.cos(deg2rad(to_lat)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+
+  return d;
+}
+
+function deg2rad(deg)
+{
+  return deg * (Math.PI/180);
+}
